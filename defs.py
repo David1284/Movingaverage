cda55d693e69176fbe92e8ad1d6397fc57b2728a
David1284 <36327198+David1284@users.noreply.github.com>
2023-12-07 13:05:09 +1100

    Add files via upload

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "76c075f93a8aff97d9f6615c1b336e66-6cc3342020ae632ff98182c8fc939f95"
+ACCOUNT_ID = "101-011-21254494-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
